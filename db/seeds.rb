c27c121d579293307c6bd71e94d461f347eee608
tin.nguyen <tin.nguyen@rakuten.com>
2023-07-02 17:39:25 +0900

    Cooked up some seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,38 @@ User.destroy_all
 SleepRecord.destroy_all
 Relationship.destroy_all
 
-user1 = User.create(name: 'Queen')
-user2 = User.create(name: 'Greenday')
-user3 = User.create(name: 'Led Zeppelin')
+users = [
+  "Imagine Dragons",
+  "Cardi B",
+  "The Weeknd",
+  "Billie Eilish",
+  "Bruno Mars",
+  "Bon Jovi"
+]
 
-user1.sleep_records.create(clock_in_time: DateTime.new(2023, 6, 1, 22, 0), clock_out_time: DateTime.new(2023, 6, 2, 6, 0))
-user1.sleep_records.create(clock_in_time: DateTime.new(2023, 6, 2, 23, 0), clock_out_time: DateTime.new(2023, 6, 3, 7, 0))
+users.each do |name|
+  User.create(name: name)
+end
 
-Relationship.create(follower: user2, followee: user1)
-Relationship.create(follower: user3, followee: user1)
+users_count = User.count
+
+User.all.each do |user|
+  # Randomly pick a number of followers (1 to 5)
+  num_followers = rand(1..5)
+
+  # Ramdom users to follow
+  followers = User.where.not(id: user.id).sample(num_followers)
+
+  # Give them followers
+  user.followers << followers
+end
+
+# Have a 10 days of sleep records data 
+User.all.each do |user|
+  (1..10).each do |day|
+    clock_in_time = Time.now - (day * 24 * 60 * 60) + rand(0..24).hours
+    clock_out_time = clock_in_time + rand(4..10).hours
+
+    user.sleep_records.create(clock_in_time: clock_in_time, clock_out_time: clock_out_time)
+  end
+end
