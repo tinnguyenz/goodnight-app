c45fc1d61c1e3bf2c7508ade53a6864fe8ffb50c
Jase <tin.nguyen@rakuten.com>
2023-07-03 23:10:20 +0900

    Create a self user, this way we can bypass/fake the authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::API
+  before_action :create_myself
+
+  private
+
+  def create_myself
+    unless session[:current_user_id]
+      user = User.create(name: "Myself")
+      session[:current_user_id] = user.id
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:current_user_id])
+  end
+
+  def session
+    request.session
+  end
+
+  def self.current_user
+    before_action do
+      @current_user ||= User.find_by(id: session[:current_user_id])
+    end
+  end
 end
