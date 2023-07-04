8937dd481328220c7ece09b5eae22beabd6876a5
Jase <tin.nguyen@rakuten.com>
2023-07-05 00:01:37 +0900

    Added pagination

--- Gemfile
+++ Gemfile
@@ -36,12 +36,15 @@ gem "bootsnap", require: false
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem "rack-cors"
 
+gem "will_paginate"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'rspec-rails', '~> 5.0.0'  
   gem 'shoulda-matchers', '~> 5.0'
   gem 'factory_bot_rails', '~> 6.0'
+  gem 'faker'
 end
 
 group :development do
--- app/controllers/api/v1/sleep_records_controller.rb
+++ app/controllers/api/v1/sleep_records_controller.rb
@@ -1,14 +1,13 @@
-class Api::V1::SleepRecordsController < ApplicationController 
+class Api::V1::SleepRecordsController < ApplicationController
   def clock_times
     user_id = params[:id] || current_user.id
-    sleep_records = SleepRecord.includes(:user).where(user_id: user_id).order(created_at: :asc)
+    page = params[:page] || 1
+    sleep_records = SleepRecord.includes(:user).where(user_id: user_id).order(created_at: :asc).paginate(page: page, per_page: 5)
 
     clock_times = sleep_records.map do |record|
       "Record from user #{record.user.name}: clock-in: #{record.clock_in_time}, clock-out: #{record.clock_out_time}"
     end
 
-    render json: { sleep_records: clock_times }
+    render json: { sleep_records: clock_times, total_pages: sleep_records.total_pages, current_page: sleep_records.current_page }
   end
 end
-
-
